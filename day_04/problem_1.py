4d5f1e62d0a02d7f021d2538fcbeac1d52df6cb3
Nico Higgs <nicoreekohiggs@gmail.com>
2022-12-03 23:22:04 -0600

    Day 4 Code

---
+++ day_04/problem_1.py
@@ -0,0 +1,21 @@
+import string
+
+with open("C:\\Users\\nicoh\\advent_of_code_2022\\day_04\\input.txt") as f: 
+    lines = f.readlines() 
+
+assignment_pairs = 0
+for line in lines:
+    line_split = line.split(",")
+    first_split = line_split[0].split("-")
+    second_split = line_split[1].split("-")
+    first_lower = int(first_split[0])
+    first_upper = int(first_split[1])
+    second_lower = int(second_split[0])
+    second_upper = int(second_split[1])
+    if (first_lower <= second_lower) and (first_upper >= second_upper):
+        assignment_pairs += 1
+    elif (second_lower <= first_lower) and (second_upper >= first_upper):
+        assignment_pairs += 1
+
+ 
+print(assignment_pairs)

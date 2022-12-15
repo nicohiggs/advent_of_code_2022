63beb88cf210de370d13a494e67b775b27bf178d
nicohiggs <nicoreekohiggs@gmail.com>
2022-12-15 09:56:36 -0600

    finished day 14

--- day_14/problem_2.py
+++ day_14/problem_2.py
@@ -3,10 +3,26 @@ import numpy as np
 with open("C:\\Users\\nicoh\\advent_of_code_2022\\day_14\\input.txt") as f: 
     lines = f.readlines()
 
+maxy = 0
+for line in lines:
+    line = line.strip()
+    points = line.split(' -> ')
+    pts = []
+    for point in points:
+        pt = eval(f"({point})")
+        if pt[1] > maxy:
+            maxy = pt[1]
+
+depth = maxy + 1
+
+grid_size = 400
+middle = grid_size // 2
+recenter = 500 - middle
+
 grid = []
-for i in range(200):
+for i in range(grid_size):
     new_row = []
-    for j in range(200):
+    for j in range(grid_size):
         new_row.append('.')
     grid.append(new_row)
 
@@ -16,7 +32,7 @@ for line in lines:
     pts = []
     for point in points:
         pt = eval(f"({point})")
-        pts.append((pt[0]-400, pt[1]))
+        pts.append((pt[0]-recenter, pt[1]))
     for i in range(1, len(pts)):
         pt1 = pts[i-1]
         pt2 = pts[i]
@@ -35,14 +51,18 @@ for line in lines:
             for j in range(pt1[0], pt2[0]+step, step):
                 grid[j][pt1[1]] = "#"
 
-starting_point = (100, 0)
+starting_point = (middle, 0)
 num_at_rest = 0
-flowing_out_bottom = False
-while not flowing_out_bottom:
+reached_start = False
+while not reached_start:
     curr_sand = starting_point
     can_move = True
     while can_move:
-        if grid[curr_sand[0]][curr_sand[1]+1] == '.': # down
+        if curr_sand[1] == depth:
+            can_move = False
+            grid[curr_sand[0]][curr_sand[1]] = 'o'
+            num_at_rest += 1
+        elif grid[curr_sand[0]][curr_sand[1]+1] == '.': # down
             curr_sand = (curr_sand[0], curr_sand[1]+1)
         elif grid[curr_sand[0]-1][curr_sand[1]+1] == '.': # bottom left
             curr_sand = (curr_sand[0]-1, curr_sand[1]+1)
@@ -53,8 +73,8 @@ while not flowing_out_bottom:
             grid[curr_sand[0]][curr_sand[1]] = 'o'
             num_at_rest += 1
         
-        if curr_sand[1] >= 199: # flowing out bottom
+        if curr_sand == starting_point: # still at starting
             can_move = False
-            flowing_out_bottom = True
+            reached_start = True
 
 print(num_at_rest)
